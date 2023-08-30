84f502f63ff5b5bbffae3e45d646f46cd4e4f4dd
Jeremiah Dunphy <72991752+JeremiahDunphy@users.noreply.github.com>
2023-08-30 19:11:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {
+          // should contain one SKill component for each
+          // web development skill that you have,
+          // customized with props.
+        }
+        <Skilllist />
+      </div>
+    </div>
+  );
+}
+
+const Avatar = () => {
+  return (
+    <div>
+      <img className="avatar" src="ai.jpg" alt="AI headShot" />
+    </div>
+  );
+};
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Jeremiah Dunphy</h1>
+      <p>
+        Military veteran who's diving headfirst into the realm of software
+        engineering. The tenacity and discipline developed during my years of
+        service are now driving my journey into coding. Beyond the screen, I'm
+        also a die-hard foodie. I enjoy Exploring the rich tapestry of flavors
+        and cultures.
+      </p>
+    </div>
+  );
+};
+
+const Skilllist = (props) => {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" emoji="💪" color="orangered" />
+      <Skill skill="JavaScript" emoji="🤚" color="yellow" />
+      <Skill skill="Python" emoji="🏄" color="orange" />
+      <Skill skill="Web Design" emoji="🏎️" color="green" />
+      <Skill skill="Git and Github" emoji="🐍" color="green" />
+      <Skill skill="React" emoji="👏" color="green" />
+    </div>
+  );
+};
+
+const Skill = (props) => {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+};
